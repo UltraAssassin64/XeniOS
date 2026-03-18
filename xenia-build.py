8689a44355d7419e0175a0d5408e7c76d711facc
UltraAssassin64 <assassintakeover64@gmail.com>
2026-03-18 20:17:01 +0000

    edited link_args

--- xenia-build.py
+++ xenia-build.py
@@ -1956,8 +1956,17 @@ def build_shaders(targets=None, config="release", target_os=None):
                     if subprocess.call(cmd) != 0:
                         print("ERROR: failed to compile Metal shader")
                         return 1
-                    link_args = [air_path, "-o", metallib_path, "-mios-version-min=16.0" if target_os == "ios"
-                    else air_path, "-o", metallib_path ]
+                    if target_os == "ios":
+                        link_args = [
+                            air_path,
+                            "-o", metallib_path,
+                            "-mios-version-min=16.0"
+                         ]
+                    else:
+                        link_args = [
+                            air_path,
+                            "-o", metallib_path
+                        ]
                     cmd = metal_tool("metallib", [link_args])
                     if subprocess.call(cmd) != 0:
                         print("ERROR: failed to link Metal library")
