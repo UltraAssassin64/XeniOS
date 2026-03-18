f18490f7d5907de536834c0746e332d806da12f9
UltraAssassin64 <assassintakeover64@gmail.com>
2026-03-18 04:04:20 +0000

    Original changes didn't work, trying again via brute force.

--- xenia-build.py
+++ xenia-build.py
@@ -2623,6 +2623,8 @@ class CleanGeneratedCommand(Command):
 # Generated files that should be excluded from linting/formatting
 GENERATED_FILES = [
     "src/xenia/ui/ui_resources_qrc.cpp",  # Qt resource file
+    "/workspaces/XeniOS/src/xenia/apu/xma_context_dumb.cc",
+    "/workspaces/XeniOS/src/xenia/apu/xma_context_dumb.h"
 ]
 
 def find_xenia_source_files():
