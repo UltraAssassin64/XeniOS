d46a67e34ea51bba413cf18e976399c831a11ebf
UltraAssassin64 <assassintakeover64@gmail.com>
2026-03-22 15:53:07 +0000

    emitter reduction

--- src/xenia/cpu/backend/a64/a64_emitter.cc
+++ src/xenia/cpu/backend/a64/a64_emitter.cc
@@ -1,16 +1,10 @@
-/**
- ******************************************************************************
- * Xenia : Xbox 360 Emulator Research Project
- ******************************************************************************
- */
-
 #include "xenia/cpu/backend/a64/a64_emitter.h"
 
+#include <algorithm>
 #include <cstring>
 
-#if defined(__APPLE__)
-#include <libkern/OSCacheControl.h>
-#endif
+#include "xenia/cpu/backend/a64/a64_backend.h"  // Include full definition
+#include "xenia/cpu/backend/a64/a64_op.h"
 
 namespace xe {
 namespace cpu {
@@ -18,34 +12,24 @@ namespace backend {
 namespace a64 {
 
 A64Emitter::A64Emitter(A64Backend* backend)
-    : processor_(backend->processor()),
-      backend_(backend),
-      code_cache_(backend->code_cache()) {
-  feature_flags_ = 0;  // TODO: set features
-
-  epilog_label_ = new oaknut::Label();
-}
+    : oaknut::VectorCodeGenerator(backend->code_cache()->code_buffer()),
+      processor_(backend->processor()),
+      code_cache_(backend->code_cache()) {}
 
 A64Emitter::~A64Emitter() = default;
 
-uint8_t* A64Emitter::current_address() const { return getCode() + getSize(); }
+uint8_t* A64Emitter::CodeEnd() const { return GetCurr(); }
 
-size_t A64Emitter::offset() const { return getSize(); }
+size_t A64Emitter::GetCodeSize() const { return GetCurrentCodeSize(); }
 
-void A64Emitter::Reset() { reset(); }
+void A64Emitter::Reset() { ResetCurr(); }
 
-void A64Emitter::FlushInstructionCache() {
-#if defined(__APPLE__)
-  sys_icache_invalidate(getCode(), getSize());
-#else
-#if defined(__GNUC__)
-  __builtin___clear_cache(reinterpret_cast<char*>(getCode()),
-                          reinterpret_cast<char*>(getCode() + getSize()));
+bool A64Emitter::ProtectCode(uint8_t* ptr, size_t length) {
+#if XE_PLATFORM_MAC
+  sys_icache_invalidate(ptr, length);
 #endif
-#endif
+  return true;
 }
-
-void A64Emitter::Finalize() { FlushInstructionCache(); }
 
 }  // namespace a64
 }  // namespace backend
