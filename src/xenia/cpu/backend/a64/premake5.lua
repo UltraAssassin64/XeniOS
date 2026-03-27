4103fc5688e85e6c2a4de3fd7048d4737809ffe5
UltraAssassin64 <assassintakeover64@gmail.com>
2026-03-27 17:50:09 -0400

    reversion i think

--- src/xenia/cpu/backend/a64/premake5.lua
+++ src/xenia/cpu/backend/a64/premake5.lua
@@ -15,7 +15,6 @@ project("xenia-cpu-backend-a64")
     "fmt",
     "xenia-base",
     "xenia-cpu",
-    "UIKit.framework",
   })
   sysincludedirs({
     project_root.."/third_party/oaknut/include",
